d6c1e550237c170cd5ea464cb22f1025957b2259
Antonio <amarquez1980@gmail.com>
2022-09-13 17:31:14 +0200

    test: configure example stack test project

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ const base = require('./jest.config.base');
 module.exports = {
   ...base,
   roots: ['<rootDir>'],
-  projects: ['<rootDir>/infrastructure/cdk'],
+  projects: [
+    '<rootDir>/infrastructure/cdk',
+    '<rootDir>/infrastructure/stacks/stack-example',
+  ],
 };
