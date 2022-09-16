5203848c9731b272cde73866c67ff6168b39991d
Antonio Márquez Pérez <me@amarquez.eu>
2022-09-16 16:51:57 +0200

    feat(Construct): make construct more customizable

--- infrastructure/constructs/construct-example/lib/example-construct.ts
+++ infrastructure/constructs/construct-example/lib/example-construct.ts
@@ -2,17 +2,19 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-interface CustomConstructProps {}
+interface CustomConstructProps {
+  visibilityDuration: number;
+}
 
 export class CustomConstruct extends Construct {
-  constructor(scope: Construct, id: string, props?: CustomConstructProps) {
+  constructor(scope: Construct, id: string, props: CustomConstructProps) {
     super(scope, id);
 
     // The code that defines your stack goes here
 
     // example resource
     const queue = new sqs.Queue(this, 'CdkQueue', {
-      visibilityTimeout: cdk.Duration.seconds(500),
+      visibilityTimeout: cdk.Duration.seconds(props.visibilityDuration),
     });
   }
 }
