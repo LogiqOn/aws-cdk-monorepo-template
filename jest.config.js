dc937e335b4df16e733282422dea88d60228134c
Antonio Márquez Pérez <me@amarquez.eu>
2022-12-05 20:18:38 +0100

    feat(cdk): update cdk infrastructure folder

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,5 @@ const base = require('./jest.config.base');
 module.exports = {
   ...base,
   roots: ['<rootDir>'],
-  projects: [
-    '<rootDir>/infrastructure/cdk',
-    '<rootDir>/infrastructure/stacks/stack-example',
-    '<rootDir>/infrastructure/constructs/construct-example',
-  ],
+  projects: ['<rootDir>/infrastructure'],
 };
